2dc48bde82e0603e9ce9b81da0f669455a47cbe8
Jeremiah Fallin <jeremiah.fallin@gmail.com>
2020-03-11 20:07:41 -0700

    Fixing up CMS.

--- src/cms/preview-templates/InvolvementPagePreview.js
+++ src/cms/preview-templates/InvolvementPagePreview.js
@@ -2,12 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import { InvolvementPageTemplate } from "../../templates/involvement-page";
 
-const InvolvementPagePreview = ({ entry, widgetFor }) => (
-  <InvolvementPageTemplate
-    title={entry.getIn(["data", "title"])}
-    content={widgetFor("body")}
-  />
-);
+const InvolvementPagePreview = ({ entry, widgetFor }) => {
+  console.log(entry);
+  return (
+    <InvolvementPageTemplate
+      title={entry.getIn(["data", "title"])}
+      content={widgetFor("body")}
+    />
+  );
+};
 
 InvolvementPagePreview.propTypes = {
   entry: PropTypes.shape({
