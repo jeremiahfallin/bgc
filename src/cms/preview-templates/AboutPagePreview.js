4bd91773f15ef4743544939ee983c6ce864c64a6
Jeremiah Fallin <jeremiah.fallin@gmail.com>
2020-03-18 19:19:36 -0700

    Trying About Page again.

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -1,19 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { AboutPageTemplate } from "../../templates/about-page";
-import showdown from "showdown";
 
 const AboutPagePreview = ({ entry, widgetFor }) => {
   const data = entry.getIn(["data"]).toJS();
-  const converter = new showdown.Converter();
 
   return (
     <AboutPageTemplate
       image={data.image}
       title={data.title}
       heading={data.heading}
-      board={converter.makeHtml(data.board)}
-      staff={converter.makeHtml(data.staff)}
+      board={data.board}
+      staff={data.staff}
     />
   );
 };
