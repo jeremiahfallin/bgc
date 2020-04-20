df55828c21c08db9bf0a59f304a62e9dca452cb6
Jeremiah <jeremiah.fallin@gmail.com>
2020-04-20 15:11:03 -0700

    Fixed some of the At Home Page

--- src/cms/preview-templates/AtHomePostPreview.js
+++ src/cms/preview-templates/AtHomePostPreview.js
@@ -5,12 +5,14 @@ import { AtHomePostTemplate } from "../../templates/sports-post";
 const AtHomePostPreview = ({ entry, widgetFor }) => {
   const tags = entry.getIn(["data", "tags"]);
   const files = entry.getIn(["data", "files"]);
+  const videos = entry.getIn(["data", "videos"]);
 
   return (
     <AtHomePostTemplate
       content={widgetFor("body")}
       description={entry.getIn(["data", "description"])}
       files={files && files.toJS()}
+      videos={videos && videos.toJS()}
       tags={tags && tags.toJS()}
       title={entry.getIn(["data", "title"])}
     />
