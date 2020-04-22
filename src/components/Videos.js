f603b9ac3f12334cab531fd855173a4800fa9fc9
Jeremiah <jeremiah.fallin@gmail.com>
2020-04-22 09:27:06 -0700

    Added more Virtual topics.

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,57 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const Videos = ({ videos, setVideo, activeVideo }) => (
-  <div className="container">
-    <div className="columns">
-      <div className="column">
-        {videos &&
-          videos.length > 1 &&
-          videos.map(video => {
-            return (
-              <button
-                className="button"
-                key={video.video}
-                onClick={e => setVideo(video)}
-              >
-                {video.text}
-              </button>
-            );
-          })}
-      </div>
-    </div>
-    <div className="columns">
-      <div
-        className="column"
-        style={{
-          position: "relative",
-          paddingBottom: "56.25%" /* 16:9 */,
-          paddingTop: 25,
-          height: 0
-        }}
-      >
-        <iframe
-          title={activeVideo.text}
+const Videos = ({ videos, setVideo, activeVideo }) => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <div classNameNameName="container">
+      {videos.length > 1 && (
+        <div
+          className={`dropdown ${active ? "is-active" : ""}`}
+          style={{ marginBottom: "20px" }}
+        >
+          <div className="dropdown-trigger">
+            <button
+              className="button"
+              aria-haspopup="true"
+              aria-controls="dropdown-menu"
+              onClick={(e) => setActive((a) => !a)}
+            >
+              <span>{activeVideo.text}</span>
+              <span className="icon is-small">
+                <i className="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </div>
+          <div className="dropdown-menu" id="dropdown-menu" role="menu">
+            <div className="dropdown-content">
+              {videos.map((video) => {
+                return (
+                  <div
+                    className="dropdown-item"
+                    style={{ cursor: "pointer" }}
+                    onClick={(e) => {
+                      setVideo({ text: video.text, video: video.video });
+                      setActive(false);
+                    }}
+                  >
+                    {video.text}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
+      <div classNameNameName="columns">
+        <div
+          classNameNameName="column"
           style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%"
+            position: "relative",
+            paddingBottom: "56.25%" /* 16:9 */,
+            paddingTop: 25,
+            height: 0,
           }}
-          frameBorder="0"
-          src={`https://youtube.com/embed/${activeVideo.id}`}
-          allowFullScreen
-        />
+        >
+          <iframe
+            title={activeVideo.text}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+            frameBorder="0"
+            src={`https://youtube.com/embed/${activeVideo.id}`}
+            allowFullScreen
+          />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 Videos.propTypes = {
   videos: PropTypes.array,
   setVideo: PropTypes.func,
-  activeVideo: PropTypes.object
+  activeVideo: PropTypes.object,
 };
 
 export default Videos;
